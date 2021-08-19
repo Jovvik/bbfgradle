90742b83d5f377f19e8f6f41a8606889e5561fc7
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-08-19 21:45:11 +0300

    Removed `BasicTypeGenerator` due to useless

--- src/main/kotlin/com/stepanov/bbf/generator/BasicTypeGenerator.kt
+++
@@ -1,9 +0,0 @@
-package com.stepanov.bbf.generator
-
-class BasicTypeGenerator {
-    fun generate() = basicTypes.random()
-
-    // floats might be too complicated, YARPGEN doesn't use them
-    // can't generate random valid chars easily
-    private val basicTypes = listOf("Int", "Long", "Float", "Double", "Boolean")
-}
