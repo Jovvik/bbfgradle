c4722cd3f7d794908f8a36b893d5570bae3d46bb
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-08-27 02:13:43 +0300

    Added random instance generator to body generator

--- src/main/kotlin/com/stepanov/bbf/generator/BodyGenerator.kt
+++ src/main/kotlin/com/stepanov/bbf/generator/BodyGenerator.kt
@@ -1,18 +1,24 @@
 package com.stepanov.bbf.generator
 
 import com.intellij.psi.PsiElement
+import com.stepanov.bbf.bugfinder.generator.targetsgenerators.RandomInstancesGenerator
+import com.stepanov.bbf.bugfinder.generator.targetsgenerators.typeGenerators.RandomTypeGenerator
 import com.stepanov.bbf.bugfinder.mutator.transformations.Factory
 import com.stepanov.bbf.generator.arithmetic.CastOperator
 import com.stepanov.bbf.generator.arithmetic.Type
 import com.stepanov.bbf.generator.arithmetic.Variable
+import org.jetbrains.kotlin.builtins.isFunctionTypeOrSubtype
+import org.jetbrains.kotlin.psi.KtCallableDeclaration
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtFunction
 
 class BodyGenerator(
     val body: KtExpression,
     val context: Context,
     val file: KtFile,
-    val returnType: KtTypeOrTypeParam?
+    val returnType: KtTypeOrTypeParam?,
+    val containingFunction: KtFunction?
 ) {
     private fun generateArithmetic(index: Int) {
         Factory.psiFactory.createProperty(
@@ -59,15 +65,17 @@ class BodyGenerator(
         when {
             returnType == null -> {
             }
-            returnVariable != null -> addToBody("return ${returnVariable.name}")
             returnType is KtTypeOrTypeParam.Parameter -> generateTodo()
+            returnVariable != null -> {
+                addToBody("return ${returnVariable.name}")
+            }
             numericReturnType != null && numericVariable != null -> {
                 addToBody("return ${CastOperator(context, 0, Variable(context, numericVariable), numericReturnType)}")
             }
             (returnType as KtTypeOrTypeParam.Type).type.isMarkedNullable -> {
                 addToBody("return null")
             }
-            else -> generateTodo()
+//            else -> generateTodo()
             // TODO: add generation from functions or properties
 //            else -> {
 //                val instance = ClassInstanceGenerator(file).generateRandomInstanceOfUserClass(returnType.type)?.first
